ff42d2585ddecbeacf08a0d6c20ad7a9168cf92b
Matt Crouch <matt@mattcrouch.net>
2019-11-10 20:24:44 +0000

    Temp - disable button 2

--- index.js
+++ index.js
@@ -2,31 +2,42 @@ const Button = require("./Button");
 const EVENTS = require("./buttonEvents");
 
 const BUTTON_1 = new Button("Button 1", "4C-EF-C0-A6-A8-69");
-const BUTTON_2 = new Button("Button 2", "00-71-47-CB-B6-3D");
+// const BUTTON_2 = new Button("Button 2", "00-71-47-CB-B6-3D");
+const BUTTON_2 = undefined;
 
-BUTTON_1.connect()
-  .then(() => {
-    BUTTON_1.on(EVENTS.DOWN, () => {
-      console.log("💥 PLAYER 1");
+if (BUTTON_1) {
+  BUTTON_1.connect()
+    .then(() => {
+      BUTTON_1.on(EVENTS.DOWN, () => {
+        console.log("1️⃣ - BUTTON 1");
+      });
+    })
+    .catch(() => {
+      BUTTON_1.close();
     });
-  })
-  .catch(() => {
-    BUTTON_1.close();
-  });
+}
 
-BUTTON_2.connect()
-  .then(() => {
-    BUTTON_2.on(EVENTS.DOWN, () => {
-      console.log("💥 PLAYER 2");
+if (BUTTON_2) {
+  BUTTON_2.connect()
+    .then(() => {
+      BUTTON_2.on(EVENTS.DOWN, () => {
+        console.log("2️⃣ - BUTTON 2");
+      });
+    })
+    .catch(() => {
+      BUTTON_2.close();
     });
-  })
-  .catch(() => {
-    BUTTON_2.close();
-  });
+}
 
 process.on("SIGINT", () => {
   console.log("Shutting down...");
-  BUTTON_1.close();
-  BUTTON_2.close();
+
+  if (BUTTON_1) {
+    BUTTON_1.close();
+  }
+  if (BUTTON_2) {
+    BUTTON_2.close();
+  }
+
   process.exit();
 });
